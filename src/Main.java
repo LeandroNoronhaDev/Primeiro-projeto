11ea914645c727bffcf30ba2e8da4e1e01288371
Leandro <leandro.noronha@ba.estudante.senai.br>
2026-06-08 14:56:34 -0300

    1.0.2 test

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ola mundo");
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+        System.out.println("Olá " + nome + ", sua idade é " + idade + " anos");
 
-        
+        if (idade >= 18){
+            System.out.println(nome + " é maior de idade!");
+        } else {
+            System.out.println("É menor de idade!");
+        }
     }
 }
